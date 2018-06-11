b51044d0a5504048d31eda49f2bf284784a7c600
Rodolfo Roque Carcamo Mesa <carcamomesa@gmail.com>
2018-06-11 22:03:55 +0000

    Configuracion conexion

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const PORT = process.env.PORT || 5000
-const path = require('path')
 
 const config = require('./server/config');
 const app = require('./server');
 
-app.use(app.static(path.join(__dirname, 'public')))
-.get('/', (req, res) => res.render('api/index'))
-.listen(PORT, () =>
+app.listen(PORT, () =>
   console.log(`Listening on ${ PORT }`))
