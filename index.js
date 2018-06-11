e4a3e64a7fc1d9ec1eba178e93e09408f78eb4cd
Rodolfo Roque Carcamo Mesa <carcamomesa@gmail.com>
2018-06-11 19:40:26 +0000

    Configuracion conexion

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const config = require('./server/config');
 const app = require('./server');
 
 app.listen(config.port, config.hostname, () => {
-  console.log(`Server running at http://${config.hostname}:${config.port}/`);
+  console.log(`Server corriendo at http://${config.hostname}:${config.port}/`);
 });
