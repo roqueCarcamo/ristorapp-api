19a4560215963a3a8432444cf0e8b55319ef10ba
Rodolfo Roque Carcamo Mesa <carcamomesa@gmail.com>
2018-06-11 18:11:03 +0000

    Configuracion conexion

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const config = { 
     hostname: process.env.IP,
-    port: process.env.PORT,
+    port: process.env.PORT || 3338,
     db: {
         url: process.env.DBURL
     },
