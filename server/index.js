55a90ab799d8708a7700cb222995e5a7bd3dee99
Rodolfo Roque Carcamo Mesa <carcamomesa@gmail.com>
2018-06-13 03:13:05 +0000

    Modificación cross

--- server/index.js
+++ server/index.js
@@ -11,10 +11,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin: config.cors.origin,
-    credentials: Boolean(config.cors.credentials)
-}));
+//app.use(cors({
+//    origin: config.cors.origin,
+//    credentials: Boolean(config.cors.credentials)
+//}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,  Authorization");
+  res.header("Access-Control-Request-Headers", "Origin, X-Requested-With, Content-Type, Accept,  Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+  next();
+});
 
 const api = require('./api/v1');
 
